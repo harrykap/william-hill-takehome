76668bc4bc4260f4d96534da93aba497c19971cf
Harry Kapsales <harrykap@gmail.com>
2020-05-26 13:17:41 -0400

    Take Home Test for William Hill Got the script font working

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,11 +1,41 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 export default class Team extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            teamInfo: {
+                strTeam: '',
+                strTeamBadge: '',
+            }
+        };
+    }
+
+
+    componentDidMount() {
+        const teamUrl = `https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${this.props.name}`;
+        fetch(teamUrl)
+            .then(result => result.json())
+            .then(data => {
+                console.log('have data', data);
+                this.setState({teamInfo: data.teams[0]})
+                return data;
+            })
+            .catch(error => {
+                console.log('there was an error', error);
+                return error;
+            })
+    }
+
     render() {
-        return(
-            <div>
-                <h1>Team</h1>
-            </div>
+        return (
+            <React.Fragment>
+                <h1>
+                    <img src={this.state.teamInfo.strTeamBadge} alt={'team badge'} className={'badge'} />
+                    <span className={'yankee-script'}>&nbsp;&nbsp;{this.state.teamInfo.strTeam}</span>
+                </h1>
+                <hr />
+            </React.Fragment>
         )
     }
 }
